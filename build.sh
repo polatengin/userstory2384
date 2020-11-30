0710199d7f6144c1e1366c8cf7ff7289a8946720
polatengin <enpolat@microsoft.com>
2020-11-30 16:23:08 +0300

    initial commit

---
+++ build.sh
@@ -0,0 +1,30 @@
+SCRIPT_FILE=$1
+BUILD_ID=$2
+PART=$3
+
+VERSION=$(date '+%Y%m%d%H%M%S')
+
+echo "
+FROM ubuntu
+
+RUN apt-get update
+
+RUN apt-get install -y curl gnupg gnupg2 ca-certificates
+
+RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+RUN echo \"deb https://dl.bintray.com/loadimpact/deb stable main\" | tee -a /etc/apt/sources.list
+RUN apt-get update
+RUN apt-get install k6
+
+WORKDIR /k6
+
+ADD ${SCRIPT_FILE} ./script.js
+
+RUN printf \"#! /bin/bash\n\nk6 run ./script.js && curl -v 'https://enpolat-k6-testing.free.beeceptor.com/api/k6-test?build_id=${BUILD_ID}&part=${PART}'\" > run.sh
+
+RUN chmod +x ./run.sh
+
+ENTRYPOINT [\"./run.sh\"]
+" | docker build --tag enpolatacr.azurecr.io/k6:${VERSION} -
+
+docker push enpolatacr.azurecr.io/k6:${VERSION}
